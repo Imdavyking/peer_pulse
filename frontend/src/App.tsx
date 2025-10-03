0bffd4c7f149526d455e76534068842e9f864753
imdavyking <yungdynamic53@gmail.com>
2025-10-03 19:10:24 +0100

    chore: updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import NavHeader from "./components/NavHeader";
 import Router from "./router";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
@@ -23,7 +22,6 @@ function App() {
         <ToastContainer />
         <BrowserRouter>
           <NavHeader />
-          {/* <WalletSelector /> */}
           <Router />
         </BrowserRouter>
       </AptosWalletAdapterProvider>
--- frontend/src/components/ConnectWalletButton.tsx
+++ frontend/src/components/ConnectWalletButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
 const ConnectWalletButton = () => {
