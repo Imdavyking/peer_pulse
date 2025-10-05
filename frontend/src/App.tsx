e09ea2901f5ca82bb51340f9613de352158c4063
imdavyking <yungdynamic53@gmail.com>
2025-10-05 23:25:20 +0100

    chore: updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,14 @@ import Router from "./router";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { PetraWallet } from "petra-plugin-wallet-adapter";
-import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
-import { PontemWallet } from "@pontem/wallet-adapter-plugin";
-import { RiseWallet } from "@rise-wallet/wallet-adapter";
 
-const wallets = [
-  new PetraWallet(),
-  new MartianWallet(),
-  new PontemWallet(),
-  new RiseWallet(),
-];
 function App() {
   return (
     <>
-      <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+      <AptosWalletAdapterProvider
+        optInWallets={["Petra", "Pontem Wallet", "Nightly", "MSafe"]}
+        autoConnect={true}
+      >
         <ToastContainer />
         <BrowserRouter>
           <NavHeader />
