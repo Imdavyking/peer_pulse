76dd72c8056336e152f631db1ceeed591273ebf0
imdavyking <yungdynamic53@gmail.com>
2025-10-06 02:31:15 +0100

    chore: updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,13 @@ import Router from "./router";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { Network } from "aptos";
 
 function App() {
   return (
     <>
       <AptosWalletAdapterProvider
+        dappConfig={{ network: Network.DEVNET }}
         optInWallets={["Petra", "Pontem Wallet", "Nightly"]}
         autoConnect={true}
       >
